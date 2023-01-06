ddf210ab85fd6102b7f2c24d38f630c3ebc7b32e
Thomas <thomaswang003@gmail.com>
2023-01-05 20:46:21 -0500

    created default home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
+import './App.css'
+
 function App() {
   return (
-    <>
-    </>
+    <div style = {{ height: "90vh ", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+      <h2 style= {{color: "white", fontWeight: "200"}}>Stock Analaysis OBV Indicator</h2>
+      <form method = "POST">
+        <input type = "text" placeholder = "Enter Stock Here" className = "searchBar"/>
+      </form>
+    </div>
   );
 }
 
